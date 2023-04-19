9bd3ece279fad03f4e93b0d4beed5a8a5b7900a9
Alex Perez <alejandro.gperez745@gmail.com>
2023-04-19 15:03:19 -0500

    changes before deployment

--- src/components/Projects/Projectied.jsx
+++ src/components/Projects/Projectied.jsx
@@ -20,18 +20,18 @@ export default function Projectied() {
             <section className='body-info'>
                 <p>
                 Projectied (pronounced: "project-tied") is a collaborative management app that allows users to create projects/lists and organize their tasks within each project. Users can add collaborators to their projects, so multiple people can work on them at once. The app offers the following features:
-                <br />
+                </p>
                 <ul>
                     <li><b>Create projects:</b> Users can create projects/lists and add tasks to them.</li>
                     <li><b>Organize tasks:</b> Within each project, users can organize tasks into three categories: Todo , Active, and Completed.</li>
                     <li><b>Collaborate with others:</b> Users can invite collaborators to their projects/lists, and everyone can work on tasks together.</li>
                 </ul>
-                </p>
+                
                 <p>My primary responsibilities on this project included implementation of the entire backend, including database and authentication, and API implementation to enable real-time updates</p>
 
                 <div className="video">
                     <h3>Watch the Live Demo!</h3>
-                    <iframe src="https://www.youtube.com/embed/U4b1pxl-634" frameborder="0"></iframe>
+                    <iframe src="https://www.youtube.com/embed/U4b1pxl-634" frameBorder="0"></iframe>
                 </div>
                 <div className='technologies'>
                     <h2>Built With</h2>
