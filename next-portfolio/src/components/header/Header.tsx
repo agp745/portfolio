866f8c9aa509e55b7528229254d793f518614ef5
Alex Perez <alexgp@Alexs-MBP.attlocal.net>
2023-06-07 12:27:22 -0500

    update with next started

---
+++ next-portfolio/src/components/header/Header.tsx
@@ -0,0 +1,11 @@
+
+
+export const Header = () => {
+    return (
+        <nav className="fixed top-0 left-0 right-0 h-10 bg-blue-300">
+            <div className="flex justify-center items-center h-full">
+                <div>Header</div>
+            </div>
+        </nav>
+    )
+}
---
+++ next-portfolio/src/components/hero/Hero.tsx
@@ -0,0 +1,10 @@
+
+export const Hero = () => {
+
+    return(
+        <section className="flex flex-col items-center gap-4 text-white brightness-105 mt-32 drop-shadow-2xl">
+            <h1 className="SHOW_TEXT_1s text-8xl font-semibold">Alejandro Perez</h1>
+            <p className="SHOW_TEXT_2s text-4xl font-light text-center tracking-[0.5rem]">software developer</p>
+        </section>
+    )
+}
